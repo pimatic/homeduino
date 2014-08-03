ea1c0dc6ca23ef641f6cf368fbc2849303df6259
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2014-08-03 18:03:38 +0200

    dht reading fixups and type handling

--- dht.h
+++ dht.h
@@ -8,30 +8,45 @@ void dht_command() {
     if(arg == NULL) {
         return;
     }
+    int dht_type = atoi(arg);
+    arg = sCmd.next();
+    if(arg == NULL) {
+        return;
+    }
     int dht_pin = atoi(arg);
-    int chk = DHT.read22(dht_pin);
 
-    switch (chk)
-    {
-    case DHTLIB_OK:
-        Serial.print("DHT data ");
-        Serial.print(dht_pin);
-        Serial.write(' ');
-        Serial.print(DHT.temperature, 1);
-        Serial.write(' ');
-        Serial.print(DHT.humidity, 1);
-        break;
-    case DHTLIB_ERROR_CHECKSUM:
-        Serial.print("DHT checksum_error ");
-        Serial.print(dht_pin);
-        break;
-    case DHTLIB_ERROR_TIMEOUT:
-        Serial.print("DHT timeout_error ");
-        Serial.print(dht_pin);
-        break;
-    default:
-        Serial.print("DHT unknown_error ");
-        Serial.print(dht_pin);
-        break;
+    // just the dht 11 sensor read function is different
+    int chk;
+    if (dht_type == 1) {
+        chk = DHT.read11(dht_pin);
+    } else {
+        chk = DHT.read(dht_pin);
+    }
+
+    switch (chk) {
+        case DHTLIB_OK:
+            Serial.print("DHT data ");
+            Serial.print(dht_pin);
+            Serial.write(' ');
+            Serial.print(DHT.temperature, 1);
+            Serial.write(' ');
+            Serial.print(DHT.humidity, 1);
+            Serial.write('\n');
+            break;
+        case DHTLIB_ERROR_CHECKSUM:
+            Serial.print("DHT checksum_error ");
+            Serial.print(dht_pin);
+            Serial.write('\n');
+            break;
+        case DHTLIB_ERROR_TIMEOUT:
+            Serial.print("DHT timeout_error ");
+            Serial.print(dht_pin);
+            Serial.write('\n');
+            break;
+        default:
+            Serial.print("DHT unknown_error ");
+            Serial.print(dht_pin);
+            Serial.write('\n');
+            break;
     }
 }
