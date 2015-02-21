0dc3540cbe2e0a18b49b2d0230c9fd750658e0ef
Ronald Steen <ronald.steen@gmail.com>
2015-02-21 14:38:34 +0100

    new commands

--- homeduino.ino
+++ homeduino.ino
@@ -25,14 +25,12 @@ void reset_command();
 void pin_mode_command();
 void unrecognized(const char *command);
 void dst_command();
+void printTemperature(int pin, byte address[8]);
+void printTemperatures(int pin);
+void printSensors(int pin);
 void printAddress(DeviceAddress deviceAddress);
-
-OneWire oneWire(ONE_WIRE_BUS);
-
-DallasTemperature sensors(&oneWire);
-
 void setup() {
-  sensors.begin();
+
 
   Serial.begin(115200);
   // Setup callbacks for SerialCommand commands
@@ -66,81 +64,81 @@ void loop() {
 
 void digital_read_command() {
   char* arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
-    int pin = atoi(arg);
-    int val = digitalRead(pin);
-    Serial.print("ACK ");
-    Serial.write('0' + val);
-    Serial.print("\r\n");
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int pin = atoi(arg);
+  int val = digitalRead(pin);
+  Serial.print("ACK ");
+  Serial.write('0' + val);
+  Serial.print("\r\n");
 }
 
 void analog_read_command() {
   char* arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
-    int pin = atoi(arg);
-    int val = analogRead(pin);
-    Serial.print("ACK ");
-    Serial.print(val);
-    Serial.print("\r\n");
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int pin = atoi(arg);
+  int val = analogRead(pin);
+  Serial.print("ACK ");
+  Serial.print(val);
+  Serial.print("\r\n");
 }
 
 void digital_write_command() {
   char* arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
-    int pin = atoi(arg);
-    arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
-    int val = atoi(arg);
-    digitalWrite(pin, val);
-    Serial.print("ACK\r\n");
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int pin = atoi(arg);
+  arg = sCmd.next();
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int val = atoi(arg);
+  digitalWrite(pin, val);
+  Serial.print("ACK\r\n");
 }
 
 void analog_write_command() {
   char* arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
-    int pin = atoi(arg);
-    arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
-    int val = atoi(arg);
-    analogWrite(pin, val);
-    Serial.print("ACK\r\n");
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int pin = atoi(arg);
+  arg = sCmd.next();
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int val = atoi(arg);
+  analogWrite(pin, val);
+  Serial.print("ACK\r\n");
 }
 
 void pin_mode_command() {
   char* arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
-    int pin = atoi(arg);
-    arg = sCmd.next();
-    if(arg == NULL) {
-      argument_error();
-      return;
-    }
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int pin = atoi(arg);
+  arg = sCmd.next();
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
     // INPUT 0x0
   // OUTPUT 0x1
-    int mode = atoi(arg);
-    pinMode(pin, mode);
-    Serial.print("ACK\r\n");  
+  int mode = atoi(arg);
+  pinMode(pin, mode);
+  Serial.print("ACK\r\n");  
 }
 
 void reset_command() {
@@ -157,51 +155,85 @@ void unrecognized(const char *command) {
 }
 
 
-//DST sensors
-//DST [address]
+//DST [pin] sensors
+//DST [pin] [address] -> single temperature C
+//DST [pin] all -> all temperatures on that pin.
 void dst_command(){
+  char* pinArg = sCmd.next();
+  if(pinArg == NULL) {
+    argument_error();
+    return;
+  }
 
-    char* arg = sCmd.next();
-    if(arg == NULL) {
-        argument_error();
-        return;
+  int pin = atoi(pinArg);
+
+  char* arg = sCmd.next();
+
+  if (strcmp(arg, "all") == 0){
+    printTemperatures(pin);
+  } else if(strcmp(arg, "sensors") == 0) {
+    printSensors(pin);
+  } else {
+    byte address[8];
+    int i;
+    uint8_t str_len = strlen(arg);
+
+    memset(address, 0x00, sizeof(address));
+    for (i = 0; i < ((str_len / 2)); i++) {
+      sscanf(arg+2*i, "%02x", &address[i]);
     }
 
-    if(strcmp(arg, "sensors") == 0) {
-      DeviceAddress address;
-            
-      oneWire.reset_search();
-      
-      Serial.print("ACK sensors ");
-      while(oneWire.search(address)){
-        printAddress(address);
-        Serial.print(" ");
-      }
-      Serial.print("\r\n");
-    } else {      
-      byte address[8];
+    printTemperature(pin, address);    
+  }
+}
 
-      int i;
-        
-      uint8_t str_len = strlen(arg);
+void printTemperature(int pin, byte address[8]){
+  OneWire oneWire(pin);
+  DallasTemperature sensors(&oneWire);
+  sensors.begin(); 
 
-      Serial.print("LEN: ");
-      Serial.print(str_len, 1);
-      Serial.print( + "\r\n");
+  sensors.requestTemperaturesByAddress(address);
+  double temp = sensors.getTempC(address); 
 
-      memset(address, 0x00, sizeof(address));
-      for (i = 0; i < ((str_len / 2)); i++) {
-          sscanf(arg+2*i, "%02x", &address[i]);
-      }
-      printAddress(address);
-      Serial.print( + "\r\n");
+  Serial.print("ACK ");
+  Serial.print(temp, 1);
+  Serial.print("\r\n");
+}
 
-      double temp = sensors.getTempC(address); 
+void printTemperatures(int pin){
+  OneWire oneWire(pin);
+  DallasTemperature sensors(&oneWire);
+  sensors.begin(); 
+  oneWire.reset_search();
+  sensors.requestTemperatures(); 
 
-      Serial.print("ACK ");
-      Serial.print(temp, 1);
-      Serial.print("\r\n");
-    }
+  int index = 0;
+  DeviceAddress address;
+  Serial.print("ACK ");
+  while (oneWire.search(address)){
+    double temp = sensors.getTempCByIndex(index++); 
+    Serial.print(temp, 1);
+    Serial.print(" ");
+  }
+  
+  Serial.print("\r\n");
+}
+
+void printSensors(int pin){
+  OneWire oneWire(pin);
+  DallasTemperature sensors(&oneWire);
+  sensors.begin(); 
+
+  DeviceAddress address;
+
+  oneWire.reset_search();
+
+  Serial.print("ACK sensors ");
+  while(oneWire.search(address)){
+    printAddress(address);
+    Serial.print(" ");
+  }
+  Serial.print("\r\n");
 }
 
 void printAddress(DeviceAddress deviceAddress)
