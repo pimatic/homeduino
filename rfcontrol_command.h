e233f107b53e3a82754cb38b9fd147b294ddf464
Ronny <ronny_herzberg@web.de>
2015-03-02 01:32:54 +0100

    add new RF raw function

--- rfcontrol_command.h
+++ rfcontrol_command.h
@@ -5,9 +5,25 @@ int interrupt_pin = -1;
 
 void rfcontrol_command_send();
 void rfcontrol_command_receive();
+void rfcontrol_command_raw();
+
+bool in_raw_mode = false;
 
 void rfcontrol_loop() {
-  if(RFControl::hasData()) {
+  if(in_raw_mode){
+    if(RFControl::existNewDuration()){
+      Serial.print(RFControl::getLastDuration());
+      Serial.write(' ');
+      static byte line=0;
+      line++;
+      if(line>=16){
+        line=0;
+        Serial.write('\n');
+      }
+    }
+  }
+  else{
+    if(RFControl::hasData()) {
       unsigned int *timings;
       unsigned int timings_size;
       RFControl::getRaw(&timings, &timings_size);
@@ -15,15 +31,16 @@ void rfcontrol_loop() {
       RFControl::compressTimings(buckets, timings, timings_size);
       Serial.print("RF receive ");
       for(unsigned int i=0; i < 8; i++) {
-          Serial.print(buckets[i]);
-          Serial.write(' ');
+        Serial.print(buckets[i]);
+        Serial.write(' ');
       }
       for(unsigned int i=0; i < timings_size; i++) {
-          Serial.write('0' + timings[i]);
+        Serial.write('0' + timings[i]);
       }
       Serial.print("\r\n");
       RFControl::continueReceiving();
     }
+  }
 }
 
 void rfcontrol_command() {
@@ -36,9 +53,23 @@ void rfcontrol_command() {
     rfcontrol_command_send();
   } else if (strcmp(arg, "receive") == 0) {
     rfcontrol_command_receive();
+  } else if(strcmp(arg, "raw") == 0) {
+    rfcontrol_command_raw();
   } else {
     argument_error();
   }
+}
+
+void rfcontrol_command_raw(){
+  char* arg = sCmd.next();
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int interrupt_pin = atoi(arg);
+  RFControl::startReceiving(interrupt_pin);
+  in_raw_mode = true;
+  Serial.print("ACK\r\n");	
 }
 
 void rfcontrol_command_receive() {
@@ -49,6 +80,7 @@ void rfcontrol_command_receive() {
   }
   interrupt_pin = atoi(arg);
   RFControl::startReceiving(interrupt_pin);
+  in_raw_mode = false;
   Serial.print("ACK\r\n");
 }
 
@@ -85,5 +117,6 @@ void rfcontrol_command_send() {
     return;
   }
   RFControl::sendByCompressedTimings(transmitter_pin, buckets, arg, repeats);
+  in_raw_mode = false;
   Serial.print("ACK\r\n");
 }
